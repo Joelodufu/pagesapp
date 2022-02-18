83965e9d3134d4083a64406d98419c990e7cd93e
joelodufu <joel.odufu@gmail.com>
2022-02-18 03:57:09 +0100

    The earliest

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width:100vw;
+    height:7vh;
+    border-bottom:1px solid grey;
+`
+const Nav = () => {
+  return (
+    <Container>Nav</Container>
+  )
+}
+
+export default Nav
